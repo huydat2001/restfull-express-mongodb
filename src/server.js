0b6cff0a531e64bf528abe4d6695d7e680ca4433
nguyen huy dat <nguyenhuydatvp201@gmail.com>
2025-02-18 22:08:45 +0700

    video 104

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ const webRoutes = require("./routes/web");
 const apiRoutes = require("./routes/api");
 
 const connection = require("./config/database");
+const { MongoClient } = require("mongodb");
 
 const app = express(); // app express
 const port = process.env.PORT || 8888; //port => hardcode . uat .prod
@@ -27,7 +28,16 @@ app.use("/v1/api/", apiRoutes);
 
 (async () => {
   try {
-    await connection();
+    // using mongoose
+    // await connection();
+    // using mongodb driver
+    const url = process.env.DB_HOST_WITH_DRIVER;
+    const client = new MongoClient(url);
+    const dbName = process.env.DB_NAME;
+    await client.connect();
+    console.log("connect success");
+    const db = client.db(dbName);
+    const collection = db.collection("customers");
     app.listen(port, hostname, () => {
       console.log(`Example app listening on port ${port}`);
     });
